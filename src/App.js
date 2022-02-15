795ca47ddbd093c58a5c00b6e3ca7516cff2b92a
Jouni Juntunen <jouni.juntunen@oamk.fi>
2022-02-15 10:02:49 +0200

    Valmis sovellus.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import Weather from './components/Weather';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [lat,setLat] = useState(0);
+  const [lng, setLng] = useState(0)
+  const [isLoading,setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsLoading(false);
+      },(error) => {
+        alert(error);
+      })
+    } else {
+      alert('Your browser does not support geolocation!');
+    }
+  }, [])
+  
+  if (isLoading) {
+    return <div><p>Loading...</p></div>
+  } else {
+    return (
+      <div>
+        <h3>Your position is</h3>
+        <p>Position: {lat.toFixed(3)},{lng.toFixed(3)}</p>
+        <Weather lat={lat} lon={lng}/>
     </div>
-  );
+    );
+  }
+
+
 }
 
 export default App;
